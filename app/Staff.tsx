f51b87019baa6e640f4351cc542a4328776622fc
Fasika Dagnachew <fasika1515@gmail.com>
2024-08-01 13:30:38 +0300

    update staff images

--- app/Staff.tsx
+++ app/Staff.tsx
@@ -6,14 +6,14 @@ import Link from 'next/link'
 
 export default function Staff() {
   return (
-    <div className='w-full px-8 sm:px-14 md:px-[100px] flex flex-col gap-5'>
-        <h3 className='text-xl text-center lg:text-left sm:text-2xl md:text-3xl text-[#3a3a38] font-bold'>Meet Our Staff</h3>
-        <h2 className='text-4xl text-center lg:text-left sm:text-5xl md:text-6xl text-[#3a3a38] font-bold max-w-[800px] md:leading-[4.25rem] mb-10'>Professional lawyers With extensive experience</h2>
-        <div className="w-full flex items-center justify-center lg:justify-between gap-5 flex-wrap lg:flex-nowrap">
+    <div className='w-full px-8 sm:px-14 md:px-[100px] flex flex-col items-center gap-5'>
+        <h3 className='text-xl text-center sm:text-2xl md:text-3xl text-[#3a3a38] font-bold'>Meet Our Staff</h3>
+        <h2 className='text-4xl text-center sm:text-5xl md:text-6xl text-[#3a3a38] font-bold max-w-[800px] md:leading-[4.25rem] mb-10'>Professional lawyers With extensive experience</h2>
+        <div className="w-full flex items-center justify-center gap-20 flex-wrap lg:flex-nowrap">
             {staff?.slice(0, 3).map((member, index) => (
                 <div key={index} className="w-full sm:w-[350px] p-8 flex flex-col items-center bg-[#D9D9D9]">
-                    <Image src={member.image} className="w-full h-[300px] object-cover" alt={member.name} width={500} height={800} />
-                    <h2 className='text-4xl my-5'>{member.name}</h2>
+                    <Image src={member.image} className="w-full h-[350px] object-cover" alt={member.name} width={500} height={800} />
+                    <h2 className='text-4xl my-5 text-center'>{member.name}</h2>
                     <p className='text-[#085AA3] mb-5 text-lg'>{member.title}</p>
                     <div className="w-fit flex items-center justify-between gap-5 text-[#085AA3] text-2xl">
                         {member?.socials.map((social, i) => (
--- data/staff.tsx
+++ data/staff.tsx
@@ -6,7 +6,7 @@ const staff = [
         name: 'Eferem Hailemariam',
         position: 'CEO',
         title: 'Senior Business Lawyer',
-        image: '/images/staff/staff1.png',
+        image: '/images/staff/Eferem.jpeg',
         bio: 'Eferem represents clients in a variety of legal matters before courts of law, arbitration tribunals, and other semi-judicial tribunals. ',
         socials: [
             {
@@ -40,7 +40,7 @@ const staff = [
         name: 'Meskerem Ayalew',
         position: 'CFO',
         title: 'Senior Business Lawyer',
-        image: '/images/staff/staff2.png',
+        image: '/images/staff/Meskerem.jpeg',
         bio: 'Meskerem offers client advice and consultation on a range of business, tax, and contract-related Issues. She started her legal career as a legal aid provider at the Center for Justice at Kilinto Remand Prison Centre after earning her law degree from Hawassa University, School of Law in 2022. There she gained expertise in criminal law, criminal court proceedings, and contractual disputes. After that, she started working as a legal secretary at ELO, where she gained experience giving clients legal advice in areas about contracts, labor laws, taxes, and commercial and business matters.',
         socials: [
             {
@@ -69,74 +69,74 @@ const staff = [
             }
         ]
     },
-    {
-        id: 3,
-        name: 'John Doe',
-        position: 'CEO',
-        title: 'Senior Business Lawyer',
-        image: '/images/staff/staff3.png',
-        bio: 'John is a senior business lawyer with over 10 years of experience in the legal field. He has represented clients in a variety of legal matters before courts of law, arbitration tribunals, and other semi-judicial tribunals. John has a proven track record of success in complex legal matters and has earned a reputation as a skilled and dedicated advocate for his clients.',
-        socials: [
-            {
-                id: 1,
-                name: 'facebook',
-                link: 'https://www.facebook.com',
-                icon: <FaFacebook />
-            },
-            {
-                id: 2,
-                name: 'twitter',
-                link: 'https://www.twitter.com',
-                icon: <FaTwitter />
-            },
-            {
-                id: 3,
-                name: 'linkedin',
-                link: 'https://www.linkedin.com',
-                icon: <FaLinkedin />
-            },
-            {
-                id: 4,
-                name: 'email',
-                link: 'https://mail.google.com',
-                icon: <FaEnvelope />
-            }
-        ]
-    },
-    {
-        id: 4,
-        name: 'Jane Doe',
-        position: 'CFO',
-        title: 'Senior Business Lawyer',
-        image: '/images/staff/staff1.png',
-        bio: 'Jane is a senior business lawyer with over 10 years of experience in the legal field. She has represented clients in a variety of legal matters before courts of law, arbitration tribunals, and other semi-judicial tribunals. Jane has a proven track record of success in complex legal matters and has earned a reputation as a skilled and dedicated advocate for her clients.',
-        socials: [
-            {
-                id: 1,
-                name: 'facebook',
-                link: 'https://www.facebook.com',
-                icon: <FaFacebook />
-            },
-            {
-                id: 2,
-                name: 'twitter',
-                link: 'https://www.twitter.com',
-                icon: <FaTwitter />
-            },
-            {
-                id: 3,
-                name: 'linkedin',
-                link: 'https://www.linkedin.com',
-                icon: <FaLinkedin />
-            },
-            {
-                id: 4,
-                name: 'email',
-                link: 'https://mail.google.com',
-                icon: <FaEnvelope />
-            }
-        ]
-    },
+    // {
+    //     id: 3,
+    //     name: 'John Doe',
+    //     position: 'CEO',
+    //     title: 'Senior Business Lawyer',
+    //     image: '/images/staff/staff3.png',
+    //     bio: 'John is a senior business lawyer with over 10 years of experience in the legal field. He has represented clients in a variety of legal matters before courts of law, arbitration tribunals, and other semi-judicial tribunals. John has a proven track record of success in complex legal matters and has earned a reputation as a skilled and dedicated advocate for his clients.',
+    //     socials: [
+    //         {
+    //             id: 1,
+    //             name: 'facebook',
+    //             link: 'https://www.facebook.com',
+    //             icon: <FaFacebook />
+    //         },
+    //         {
+    //             id: 2,
+    //             name: 'twitter',
+    //             link: 'https://www.twitter.com',
+    //             icon: <FaTwitter />
+    //         },
+    //         {
+    //             id: 3,
+    //             name: 'linkedin',
+    //             link: 'https://www.linkedin.com',
+    //             icon: <FaLinkedin />
+    //         },
+    //         {
+    //             id: 4,
+    //             name: 'email',
+    //             link: 'https://mail.google.com',
+    //             icon: <FaEnvelope />
+    //         }
+    //     ]
+    // },
+    // {
+    //     id: 4,
+    //     name: 'Jane Doe',
+    //     position: 'CFO',
+    //     title: 'Senior Business Lawyer',
+    //     image: '/images/staff/staff1.png',
+    //     bio: 'Jane is a senior business lawyer with over 10 years of experience in the legal field. She has represented clients in a variety of legal matters before courts of law, arbitration tribunals, and other semi-judicial tribunals. Jane has a proven track record of success in complex legal matters and has earned a reputation as a skilled and dedicated advocate for her clients.',
+    //     socials: [
+    //         {
+    //             id: 1,
+    //             name: 'facebook',
+    //             link: 'https://www.facebook.com',
+    //             icon: <FaFacebook />
+    //         },
+    //         {
+    //             id: 2,
+    //             name: 'twitter',
+    //             link: 'https://www.twitter.com',
+    //             icon: <FaTwitter />
+    //         },
+    //         {
+    //             id: 3,
+    //             name: 'linkedin',
+    //             link: 'https://www.linkedin.com',
+    //             icon: <FaLinkedin />
+    //         },
+    //         {
+    //             id: 4,
+    //             name: 'email',
+    //             link: 'https://mail.google.com',
+    //             icon: <FaEnvelope />
+    //         }
+    //     ]
+    // },
 ]
 
 export default staff;
